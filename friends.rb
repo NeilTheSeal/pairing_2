1b4f97924b70f58e88d70a32c237591a1831cd71
Neil Hendren <neil.hendren@gmail.com>
2023-07-12 14:13:23 -0600

    Add an array and array method

---
+++ friends.rb
@@ -0,0 +1,11 @@
+arrays = ["Tom", "Roland", "Amith", "Jack", "Alex"];
+
+puts arrays[2];
+puts arrays[4];
+puts arrays[0];
+
+places = ["Los Angeles", "San Diego", "Paris", "Rome", "South Africa"];
+
+places.pop;
+
+print places;
